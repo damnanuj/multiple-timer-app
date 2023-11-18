11baa839015ff5df07b71b8ca27fb907da8f2eef
Anuj <anujkrgupta21@gmail.com>
2023-11-18 20:22:45 +0530

    timer app ui finished non responsive

---
+++ script.js
@@ -0,0 +1,36 @@
+//
+//
+//
+//
+//
+//
+//
+
+const hours = document.querySelector(".hour");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".second");
+
+const setBtn = document.querySelector(".set");
+const deleteBtn = document.querySelector(".delete");
+
+setBtn.addEventListener("click", () => {
+  console.log(seconds.value);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
