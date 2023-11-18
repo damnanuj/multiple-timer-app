5ff36b60e8f817e26f50e607f85260a9c415d05d
Anuj <anujkrgupta21@gmail.com>
2023-11-18 21:34:05 +0530

    js code added , non responsive

--- script.js
+++ script.js
@@ -6,31 +6,65 @@
 //
 //
 
-const hours = document.querySelector(".hour");
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".second");
 
-const setBtn = document.querySelector(".set");
-const deleteBtn = document.querySelector(".delete");
+let activeTimers = document.querySelector(".activeTimers");
 
-setBtn.addEventListener("click", () => {
-  console.log(seconds.value);
-});
+function setTimer() {
+  let hours = parseInt(document.querySelector(".hours").value);
+  let minutes = parseInt(document.querySelector(".minutes").value);
+  let seconds = parseInt(document.querySelector(".seconds").value);
 
+  let totalTimeInSeconds = hours * 3600 + minutes * 60 + seconds;
 
+  if (totalTimeInSeconds > 0) {
+    createTimer(totalTimeInSeconds);
+  } else {
+    alert("Please enter a valid time.");
+  }
+}
 
+function createTimer(totalTimeInSeconds) {
+  let timerElement = document.createElement("div");
+  timerElement.className = "active";
+  activeTimers.appendChild(timerElement);
 
+  let timeLeftTitle = document.createElement("p");
+  timeLeftTitle.textContent = "Time Left:";
+  timerElement.appendChild(timeLeftTitle);
 
+  let timeLeft = document.createElement("p");
+  timeLeft.className = "timeLeft";
+  timerElement.appendChild(timeLeft);
 
+  let deleteButton = document.createElement("p");
+  deleteButton.className = "delete";
+  deleteButton.textContent = "Delete";
+  deleteButton.onclick = function () {
+    clearInterval(timerInterval);
+    activeTimers.removeChild(timerElement);
+  };
+  timerElement.appendChild(deleteButton);
 
+  let timeRemaining = totalTimeInSeconds;
 
+  function updateTimerDisplay() {
+    let hours = Math.floor(timeRemaining / 3600);
+    let minutes = Math.floor((timeRemaining % 3600) / 60);
+    let seconds = timeRemaining % 60;
 
+    timeLeft.textContent = `${hours}h ${minutes}m ${seconds}s`;
 
+    if (timeRemaining === 0) {
+      clearInterval(timerInterval);
+      timeLeft.textContent = "Time Up!";
 
+      // Add your code to play alert music here
+    }
 
+    timeRemaining--;
+  }
 
+  updateTimerDisplay(); // Display initial time
 
-
-
-
-
+  let timerInterval = setInterval(updateTimerDisplay, 1000);
+}
